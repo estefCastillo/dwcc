24ca7a565d63ddb809d27b99e3da8e89876d9308
estefCastillo <rocsamcastillo@gmail.com>
2025-03-06 19:21:05 +0100

    06/03

--- AJAX/RecepcionProductos/scripts/index-ajax.js
+++ AJAX/RecepcionProductos/scripts/index-ajax.js
@@ -1,91 +1,91 @@
 const $d = document,
-   $category = $d.querySelector("#category"),
-   $condition = $d.querySelector("#condition"),
-   $tableBody = $d.querySelector("#tableBody"),
-   $search = $d.querySelector("#search"),
-   $tableFoot = $d.querySelector("#tableFoot"),
-   $form = $d.querySelector("form");
+  $category = $d.querySelector("#category"),
+  $condition = $d.querySelector("#condition"),
+  $tableBody = $d.querySelector("#tableBody"),
+  $search = $d.querySelector("#search"),
+  $tableFoot = $d.querySelector("#tableFoot"),
+  $form = $d.querySelector("form");
 
 let categories = [
-      {
-         name: "Teléfono",
-         id: "1",
-      },
-      {
-         name: "TV",
-         id: "2",
-      },
-      {
-         name: "Portátil",
-         id: "3",
-      },
-   ],
-   products = [
-      {
-         name: "Samsung",
-         price: "200",
-         categoryId: "1",
-         conditionId: "1",
-         id: 1,
-      },
-      {
-         name: "LG",
-         price: "300",
-         categoryId: "2",
-         conditionId: "2",
-         id: 2,
-      },
-      {
-         name: "Motorola",
-         price: "350",
-         categoryId: "3",
-         conditionId: "3",
-         id: 3,
-      },
-   ],
-   conditions = [
-      {
-         name: "Excelente",
-         id: "1",
-      },
-      {
-         name: "Bueno",
-         id: "2",
-      },
-      {
-         name: "Malo",
-         id: "3",
-      },
-   ];
+    {
+      name: "Teléfono",
+      id: "1",
+    },
+    {
+      name: "TV",
+      id: "2",
+    },
+    {
+      name: "Portátil",
+      id: "3",
+    },
+  ],
+  products = [
+    {
+      name: "Samsung",
+      price: "200",
+      categoryId: "1",
+      conditionId: "1",
+      id: 1,
+    },
+    {
+      name: "LG",
+      price: "300",
+      categoryId: "2",
+      conditionId: "2",
+      id: 2,
+    },
+    {
+      name: "Motorola",
+      price: "350",
+      categoryId: "3",
+      conditionId: "3",
+      id: 3,
+    },
+  ],
+  conditions = [
+    {
+      name: "Excelente",
+      id: "1",
+    },
+    {
+      name: "Bueno",
+      id: "2",
+    },
+    {
+      name: "Malo",
+      id: "3",
+    },
+  ];
 
 function renderCategories(categories) {
-   $category.innerHTML = categories
-      .map(
-         (el) => `
+  $category.innerHTML = categories
+    .map(
+      (el) => `
         <option value="${el.id}">${el.name}</option>
     `
-      )
-      .join("");
+    )
+    .join("");
 }
 
 function renderCondiciones(conditions) {
-   $condition.innerHTML = conditions
-      .map(
-         (el) => `
+  $condition.innerHTML = conditions
+    .map(
+      (el) => `
         <option class="bg-success text-white" value="${el.id}">
             ${el.name}
           </option>
         `
-      )
-      .join("");
+    )
+    .join("");
 }
 
 function renderProductos(products) {
-   $tableBody.innerHTML = products
-      .map((el) => {
-         let category = categories.find((c) => c.id == el.categoryId).name;
-         let estado = conditions.find((c) => c.id == el.conditionId).name;
-         return `
+  $tableBody.innerHTML = products
+    .map((el) => {
+      let category = categories.find((c) => c.id == el.categoryId).name;
+      let estado = conditions.find((c) => c.id == el.conditionId).name;
+      return `
             <tr>
               <td>${el.id}</td>
               <td>${el.name}</td>
@@ -98,12 +98,13 @@ function renderProductos(products) {
             </td>
             </tr>
     `;
-      })
-      .join("");
+    })
+    .join("");
+  renderFooter();
 }
 function renderFooter() {
-   if (products.length != 0) {
-      $tableFoot.innerHTML = `
+  if (products.length != 0) {
+    $tableFoot.innerHTML = `
       <tr>
                    <td colspan="6">
                        <button
@@ -116,37 +117,47 @@ function renderFooter() {
                    </td>
                </tr>
       `;
-   } else {
-      $tableFoot.innerHTML = `
+  } else {
+    $tableFoot.innerHTML = `
                <tr>
                 <td colspan="6">
                  <h4 class="text-white bg-danger">No hay productos</h4>
                 </td>
             </tr>`;
-   }
+  }
 }
 $d.addEventListener("DOMContentLoaded", (ev) => {
-   renderCategories(categories);
-   renderCondiciones(conditions);
-   renderProductos(products);
-   renderFooter();
+  renderCategories(categories);
+  renderCondiciones(conditions);
+  renderProductos(products);
 });
 $search.addEventListener("input", (ev) => {
-   ev.preventDefault();
-   const busqueda = ev.target.value.toLowerCase();
-   const resultados = products.filter((el) =>
-      el.name.toLowerCase().includes(busqueda)
-   );
-   renderProductos(resultados);
+  ev.preventDefault();
+  const busqueda = ev.target.value.toLowerCase();
+  const resultados = products.filter((el) =>
+    el.name.toLowerCase().includes(busqueda)
+  );
+  renderProductos(resultados);
 });
 $tableBody.addEventListener("click", (ev) => {
-   ev.preventDefault();
-   if (ev.target.classList.contains("fa-trash")) {
-      let index = products.findIndex((el) => el.id == ev.target.dataset.id);
-      products.splice(index, 1);
-      renderProductos(products);
-   }
-   if (ev.target.classList.contains("fa-pencil")) {
-      console.log(ev.target);
-   }
+  ev.preventDefault();
+  if (ev.target.classList.contains("fa-trash")) {
+    let index = products.findIndex((el) => el.id == ev.target.dataset.id);
+    products.splice(index, 1);
+    renderProductos(products);
+  }
+  if (ev.target.classList.contains("fa-pencil")) {
+    console.log(ev.target);
+  }
+});
+
+$tableFoot.addEventListener("click", (ev) => {
+  if (ev.target.id == "btnVaciar") {
+    products.length = 0;
+    renderProductos(products);
+  }
+});
+
+$form.addEventListener("submit", (ev) => {
+  ev.preventDefault();
 });
