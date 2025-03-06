789de502b554d99155b17cbc059932f7dc96c36c
estefCastillo <rocsamcastillo@gmail.com>
2025-03-06 10:40:19 +0100

    06/03

--- AJAX/RecepcionProductos/scripts/index-ajax.js
+++ AJAX/RecepcionProductos/scripts/index-ajax.js
@@ -1,7 +1,10 @@
 const $d = document,
    $category = $d.querySelector("#category"),
    $condition = $d.querySelector("#condition"),
-   $tableBody = $d.querySelector("#tableBody");
+   $tableBody = $d.querySelector("#tableBody"),
+   $search = $d.querySelector("#search"),
+   $tableFoot = $d.querySelector("#tableFoot"),
+   $form = $d.querySelector("form");
 
 let categories = [
       {
@@ -77,13 +80,73 @@ function renderCondiciones(conditions) {
       .join("");
 }
 
-function renderProductos(products){
-    $tableBody.innerHTML=products.map(el=>
-        let categories=categories.find(c.id==el.categoryId)
-    return `
-    `).join('')
+function renderProductos(products) {
+   $tableBody.innerHTML = products
+      .map((el) => {
+         let category = categories.find((c) => c.id == el.categoryId).name;
+         let estado = conditions.find((c) => c.id == el.conditionId).name;
+         return `
+            <tr>
+              <td>${el.id}</td>
+              <td>${el.name}</td>
+              <td>${el.price}</td>
+              <td>${category}</td>
+              <td>${estado}</td>
+              <td>
+              <i class="fa-solid fa-trash" data-id="${el.id}"></i> 
+               <i class="fa-solid fa-pencil" data-id="${el.id}"></i>
+            </td>
+            </tr>
+    `;
+      })
+      .join("");
+}
+function renderFooter() {
+   if (products.length != 0) {
+      $tableFoot.innerHTML = `
+      <tr>
+                   <td colspan="6">
+                       <button
+                           id="btnVaciar"
+                           class="btn mb-1 mb-sm-0 btn-outline-danger"
+                           type="button"
+                       >
+                           Borrar Todos Los Productos
+                       </button>
+                   </td>
+               </tr>
+      `;
+   } else {
+      $tableFoot.innerHTML = `
+               <tr>
+                <td colspan="6">
+                 <h4 class="text-white bg-danger">No hay productos</h4>
+                </td>
+            </tr>`;
+   }
 }
 $d.addEventListener("DOMContentLoaded", (ev) => {
    renderCategories(categories);
    renderCondiciones(conditions);
+   renderProductos(products);
+   renderFooter();
+});
+$search.addEventListener("input", (ev) => {
+   ev.preventDefault();
+   const busqueda = ev.target.value.toLowerCase();
+   const resultados = products.filter((el) =>
+      el.name.toLowerCase().includes(busqueda)
+   );
+   renderProductos(resultados);
+});
+$tableBody.addEventListener("click", (ev) => {
+   ev.preventDefault();
+   if (ev.target.classList.contains("fa-trash")) {
+      let index = products.findIndex((el) => el.id == ev.target.dataset.id);
+      products.splice(index, 1);
+      renderProductos(products);
+   }
+   if (ev.target.classList.contains("fa-pencil")) {
+      console.log(ev.target);
+   }
 });
